32b69f82517077c3001dd72b2400a79a57f261cf
Ikenna Omekam <iomekam@github.com>
2021-03-27 22:55:02 -0400

    move to fulfillment

--- app/models/fulfillment.rb
+++ app/models/fulfillment.rb
@@ -6,4 +6,24 @@ class Fulfillment
   field :status, type: String, default: "OPEN"
 
   belongs_to :order
+
+  after_create :publish_create
+  after_update :publish_update
+
+  private
+
+  def publish_create
+    kitchen_fulfillment = fulfillments.find_by(type: "KITCHEN")
+    PublishJob.perform_later("core.fulfillment.created", {
+      id: self.to_global_id.to_s,
+      user_id: user.to_global_id.to_s,
+      kitchen_fulfillment_id: kitchen_fulfillment.to_global_id.to_s
+    })
+  end
+
+  def publish_update
+    PublishJob.perform_later("core.fulfillment.updated", {
+      id: self.to_global_id.to_s
+    })
+  end
 end
