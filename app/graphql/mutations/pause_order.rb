b122e0c5f52ad68e33bd50b61427597f7a5b3849
Ikenna Omekam <iomekam@github.com>
2021-03-28 05:11:59 -0400

    add pause and cancel

---
+++ app/graphql/mutations/pause_order.rb
@@ -0,0 +1,15 @@
+module Mutations
+  class PauseOrder < BaseMutation
+    argument :fulfillment_id, ID, required: true, loads: Types::FulfillmentType
+
+    field :fulfillment, Types::FulfillmentType, null: false
+
+    def resolve(fulfillment:)
+      fulfillment.update!(request_status: "PAUSE")
+
+      {
+        fulfillment: fulfillment
+      }
+    end
+  end
+end
--- app/graphql/types/fulfillment_request_status.rb
+++ app/graphql/types/fulfillment_request_status.rb
@@ -2,4 +2,6 @@ class Types::FulfillmentRequestStatus < Types::BaseEnum
   value "SUBMITTED", "The fulfillment order has been submitted."
   value "ACCEPTED", "The fulfillment order has been accepted."
   value "CLOSED", "The fulfillment order has been completd."
+  value "PAUSED", "The fulfillment order has been paused."
+  value "CANCELLATION_REQUESTED", "Cancellation has been requested."
 end
