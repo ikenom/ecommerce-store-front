a5913e7acf85ac85254da7e14d14c686bc48cd85
Ikenna Omekam <iomekam@github.com>
2021-03-27 22:30:31 -0400

    publish update

--- app/models/order.rb
+++ app/models/order.rb
@@ -19,6 +19,7 @@ class Order
   has_many :fulfillments
 
   after_create :publish_create
+  after_update :publish_update
 
   class << self
     def from_shopify(shopify_order, draft_order)
@@ -54,9 +55,17 @@ class Order
   end
 
   def publish_create
+    kitchen_fulfillment = fulfillments.find_by(type: "KITCHEN")
     PublishJob.perform_later("core.order.created", {
       id: self.to_global_id.to_s,
-      user_id: user.to_global_id.to_s
+      user_id: user.to_global_id.to_s,
+      kitchen_fulfillment_id: kitchen_fulfillment.to_global_id.to_s
+    })
+  end
+
+  def publish_update
+    PublishJob.perform_later("core.order.updated", {
+      id: self.to_global_id.to_s
     })
   end
 end
